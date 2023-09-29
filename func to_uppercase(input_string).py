ccee068e98ad23290dad6326642ae9d44ed105d4
katoDar <katomyr@gmail.com>
2023-09-30 02:05:15 +0300

    def to_uppercase(input_string)

--- func to_uppercase(input_string).py
+++ func to_uppercase(input_string).py
@@ -1,3 +1,14 @@
 def to_uppercase(input_string):
     uppercase_string = input_string.upper()
     return uppercase_string
+
+
+def title_words(input_string):
+    """
+    Делает заглавными первые буквы
+    каждого слова в строке
+    """
+    words = input_string.title()
+    return words
+
+#print(title_words('мир во всем мире'))
