c342a3d28155a919acc8a26a38a6cfa4b97d4b06
katebzhenikova <146032422+katebzhenikova@users.noreply.github.com>
2023-10-02 16:58:50 +0300

    Update func to_uppercase(input_string).py

--- func to_uppercase(input_string).py
+++ func to_uppercase(input_string).py
@@ -1,4 +1,5 @@
 def to_uppercase(input_string):
+    """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     uppercase_string = input_string.upper()
     return uppercase_string
 
