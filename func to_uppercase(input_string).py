3562eb2954f18149c7368c60c817dd048ac5cef5
katebzhenikova <146032422+katebzhenikova@users.noreply.github.com>
2023-10-07 23:51:20 +0300

    Update func to_uppercase(input_string).py

--- func to_uppercase(input_string).py
+++ func to_uppercase(input_string).py
@@ -1,16 +1,10 @@
 def to_uppercase(input_string):
-
+    """
+    принимает на вход строку и возвращает ее со всеми заглавными буквами
+    """
     uppercase_string = input_string.upper()
     return uppercase_string
 
-def title_words(input_string):
-    """
-    Делает заглавными первые буквы каждого слова
-    в строке
-    """
-    words = input_string.title()
-    return words
 
-# print(title_words('мир во всем мире'))
 
 
